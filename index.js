08a5f4a015b07f5ae08c864d1182e4b48e253e46
MDAM182 <mdam2345@gmail.com>
2021-04-07 18:34:33 -0400

    connect api, get & post request

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const endPoint = "http://localhost:3000/api/v1/syllabuses"
 
 document.addEventListener('DOMContentLoaded', () => {
-  const endPoint = "http://localhost:3000/api/v1/notes"
   getSyllabi()
-
-  const createSyllabusForm = document.querySelector("#create-syllabus-form")
-
+  let createSyllabusForm = document.querySelector("#create-syllabus-form")
   createSyllabusForm.addEventListener("submit", (e) =>
   createFormHandler(e))
 })
@@ -42,9 +39,10 @@ function createFormHandler(e) {
 }
 
 function postFetch(title, description, image_url, category_id) {
+  console.log(title, description, image_url, category_id)
   // build my body object outside of my fetch
   const bodyData = {title, description, image_url, category_id}
-
+  //
   fetch(endPoint, {
     // POST request
     method: "POST",
@@ -54,16 +52,16 @@ function postFetch(title, description, image_url, category_id) {
   .then(response => response.json())
   .then(syllabus => {
     console.log(syllabus);
-    // const syllabusData = syllabus.data.attributes
-    // // render JSON response
-    // const syllabusMarkup = `
-    // <div data-id=${syllabus.id}>
-    //   <img src=${syllabusData.image_url} height="200" width="250">
-    //   <h3>${syllabusData.title}</h3>
-    //   <p>${syllabusData.category.name}</p>
-    //   <button data-id=${syllabusData.id}>edit</button>
-    // </div>
-    // <br><br>`;
+    const syllabusData = syllabus.data.attributes
+    // render JSON response
+    const syllabusMarkup = `
+    <div data-id=${syllabus.id}>
+      <img src=${syllabusData.image_url} height="200" width="250">
+      <h3>${syllabusData.title}</h3>
+      <p>${syllabusData.category.name}</p>
+      <button data-id=${syllabusData.id}>edit</button>
+    </div>
+    <br><br>`;
 
     document.querySelector('#syllabus-container').innerHTML += syllabusMarkup;
   })
