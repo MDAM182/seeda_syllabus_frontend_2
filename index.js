27e5757ad7c46c6fb375b1428753e837d8f908e5
MDAM182 <mdam2345@gmail.com>
2021-04-07 23:59:10 -0400

    updated

--- index.js
+++ index.js
@@ -13,20 +13,25 @@ function getSyllabi() {
     .then(syllabi => {
       // remember our JSON data is a bit nested due to our serializer
       syllabi.data.forEach(syllabus => {
-        // double check how your data is nested in the console so you can successfully access the attributes of each individual object
-        const syllabusMarkup = `
-          <div data-id=${syllabus.id}>
-            <img src=${syllabus.attributes.image_url} height="200" width="250">
-            <h3>${syllabus.attributes.title}</h3>
-            <p>${syllabus.attributes.category.name}</p>
-            <button data-id=${syllabus.id}>edit</button>
-          </div>
-          <br><br>`;
-
-          document.querySelector('#syllabus-container').innerHTML += syllabusMarkup
+        // double check how your data is nested in the console so you can
+        // successfully access the attributes of each individual object
+        render(syllabus)
       })
     })
   }
+
+  function render(syllabus){
+    const syllabusMarkup = `
+      <div data-id=${syllabus.id}>
+        <img src=${syllabus.attributes.image_url} height="200" width="250">
+        <h3>${syllabus.attributes.title}</h3>
+        <p>${syllabus.attributes.category.name}</p>
+        <button data-id=${syllabus.id}>edit</button>
+      </div>
+      <br><br>`;
+
+      document.querySelector('#syllabus-container').innerHTML += syllabusMarkup
+}
 
 
 function createFormHandler(e) {
@@ -50,19 +55,11 @@ function postFetch(title, description, image_url, category_id) {
     body: JSON.stringify(bodyData)
   })
   .then(response => response.json())
+  // .catch(err => console.log(err))
   .then(syllabus => {
     console.log(syllabus);
-    const syllabusData = syllabus.data.attributes
+    const syllabusData = syllabus.data
     // render JSON response
-    const syllabusMarkup = `
-    <div data-id=${syllabus.id}>
-      <img src=${syllabusData.image_url} height="200" width="250">
-      <h3>${syllabusData.title}</h3>
-      <p>${syllabusData.category.name}</p>
-      <button data-id=${syllabusData.id}>edit</button>
-    </div>
-    <br><br>`;
-
-    document.querySelector('#syllabus-container').innerHTML += syllabusMarkup;
+    render(syllabus)
   })
 }
